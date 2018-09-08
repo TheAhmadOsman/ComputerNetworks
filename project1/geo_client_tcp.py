3be07260ea8a2b7d9f55c4ae7590bdfc7049aaff
Ahmad-Magdy-Osman <ahmad.osman@luther.edu>
2018-09-08 14:47:34 -0500

    Completed Project 1.

--- project1/geo_client_tcp.py
+++ project1/geo_client_tcp.py
@@ -11,8 +11,20 @@ PORT = 4300
 
 def client():
     '''Main client loop'''
-    # TODO: Implement client-side tasks
-    pass
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        print('>You are connected to {}:{}'.format(HOST, PORT))
+        country = input('>Enter a country or BYE to quit\n')
+        while country.upper() != "BYE":
+            s.sendall(country.encode())
+            data = s.recv(1024)
+            if data.decode() == "NOT FOUND":
+                print("-There is no such country")                
+            else:
+                print('+{}'.format(data.decode()))
+            country = input('>Enter another country to try again or BYE to quit\n')
+        s.close()
+        print('Connection ')
 
 def main():
     '''Main function'''
--- project1/geo_server_tcp.py
+++ project1/geo_server_tcp.py
@@ -4,6 +4,7 @@ GEO TCP Server
 #!/usr/bin/env python3
 
 from socket import socket, AF_INET, SOCK_STREAM
+import time
 
 FILE_NAME = 'geo_world.txt'
 HOST = 'localhost'
@@ -13,13 +14,42 @@ PORT = 4300
 def read_file(filename: str) -> dict:
     '''Read world territories and their capitals from the provided file'''
     world = dict()
+
+    print(time.strftime('%H:%M:%S Reading a file'))
+    
+    t = time.process_time()
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            line = line.split('-')
+            world[line[0].strip()] = line[1].strip()
+    elapsed_time = time.process_time() - t
+    
+    print(time.strftime('%H:%M:%S'), "Read in {0:.5f}".format(elapsed_time), "sec")
+
     return world
 
 
 def server(world: dict) -> None:
     '''Main server loop'''
-    # TODO: Implement server-side tasks
-    pass
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(1)
+        print(time.strftime('%H:%M:%S'), 'Listening on {}:{}'.format(HOST, PORT))
+        conn, addr = s.accept()
+        with conn:
+            print(time.strftime('%H:%M:%S'), 'Connected: {}'.format(addr[0]))
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    print(time.strftime('%H:%M:%S'), 'Disconnected: {}'.format(addr[0]))
+                    break
+                country = data.decode()
+                print(time.strftime('%H:%M:%S'), 'User Query: {}'.format(country))
+                if country in world:
+                    conn.sendall(world[country].encode('utf-8'))
+                else:
+                    conn.sendall("NOT FOUND".encode('utf-8'))
 
 
 def main():
