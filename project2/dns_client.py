6e4a97b406cff7d345a5378595e3535ee70493c9
Roman Yasinovskyy <yasiro01@luther.edu>
2018-09-17 10:18:48 -0500

    Project 2 template

---
+++ project2/dns_client.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#encoding: UTF-8
+
+import sys
+
+
+class DNSClient:
+    '''DNS Client class'''
+    pass
+
+def main(argv):
+    print('Project 2')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
