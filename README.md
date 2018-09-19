f197e238b4bef887aa663bddd88772d4b1caff53
Roman Yasinovskyy <yasiro01@luther.edu>
2018-09-19 01:57:24 -0500

    Add Project 2 template

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# net-class-pub
-Computer Networks (CS430)
+# Custom dig
+
+Complete the following programming project and push code to your GitHub repository.
+
+**Process records of type A (IPv4) or AAAA (IPv6) only. If a server returns CNAME record instead, ignore it.**
+
+Use *yahoo.com* as an example of a clean and simple response.
+
+Read record type (**A** or **AAAA**), domain name, and an optional DNS server as parameters passed to your program.
+
+```
+python3 resolver.py A luther.edu 1.1.1.1
+```
+or
+```
+python3 resolver.py AAAA yahoo.com
+```
+
+1. Format a DNS request using the following values:
+
+    * Transaction ID: auto-incremental or random
+    * Flags: standard query (recursion bit set to 1, other bits are 0)
+    * Questions: 1
+    * Answer RRs: 0
+    * Authority RRs: 0
+    * Additional RRs: 0
+    * Name: user-provided
+    * Type: user-provided
+    * Class: IN (0x0001)
+
+2. If the DNS address is not specified, pick one of the well-known public servers.
+
+3. Create a UDP socket connection and send the request to the DNS server.
+
+4. Parse the response message from the DNS server to extract all addresses (there could be more than 1).
+
+5. Print the address of the DNS server used.
+
+6. Display the received information in the following format:
+
+```
+Name: <domain name>
+TTL: record time-to-live
+Addresses: <list of addresses received>
+```
+
+7. Pass all tests provided.
+
+```
+python3 -m pytest test_resolver.py
+```
+
+## Approach
+
+* Look at a valid DNS request (eg. ping www.luther.edu and capture the traffic)
+
+![DNS request](dns_query.png)
+
+* Analyze the structure of a message (see the links below for details) and replicate it
+
+![DNS request](dns_query_hex.png)
+
+* Format your own message, byte by byte (you may want to use Python's bytearray for that)
+
+* Make your client format a message based on user input (domain, record type)
+
+* Send the message and receive the response
+
+* Parse the response and present the result (IP address). Consider simple cases (domain - one or more address(es)), ignore complex paypal-like resolutions with multiple pseudos.
+
+## Resources
+
+* [RFC 1035 - Domain names - implementation and specification](https://tools.ietf.org/html/rfc1035)
+
+* [The TCP/IP Guide - DNS Messaging and Message, Resource Record and Master File Formats](http://www.tcpipguide.com/free/t_DNSMessagingandMessageResourceRecordandMasterFileF.htm)
+
+* [Chapter 15 DNS Messages](http://www.zytrax.com/books/dns/ch15/)
+
+* [Domain Name System (DNS) Parameters](http://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml)
---
+++ project2/resolver.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import sys
+from random import randint, choice, seed
+from socket import socket, SOCK_DGRAM, AF_INET
+
+
+PORT = 53
+
+DNS_TYPES = {
+    'A': 1,
+    'AAAA': 28,
+    'CNAME': 5,
+    'MX': 15,
+    'NS': 2,
+    'PTR': 12,
+    'TXT': 16
+}
+
+PUBLIC_DNS_SERVER = [
+    '1.0.0.1',  # Cloudflare
+    '1.1.1.1',  # Cloudflare
+    '8.8.4.4',  # Google
+    '8.8.8.8',  # Google
+    '8.26.56.26',  # Comodo
+    '8.20.247.20',  # Comodo
+    '9.9.9.9',  # Quad9
+    '64.6.64.6',  # Verisign
+    '208.67.222.222',  # OpenDNS
+    '208.67.220.220'  # OpenDNS
+]
+
+
+def val_to_2_bytes(value: int) -> list:
+    '''Split a value into 2 bytes'''
+    raise NotImplementedError
+
+def val_to_n_bytes(value: int, n_bytes: int) -> list:
+    '''Split a value into n bytes'''
+    raise NotImplementedError
+
+def bytes_to_val(bytes_lst: list) -> int:
+    '''Merge 2 bytes into a value'''
+    raise NotImplementedError
+
+def get_2_bits(bytes: list) -> int:
+    '''Extract first two bits of a two-byte sequence'''
+    raise NotImplementedError
+
+def get_offset(bytes: list) -> int:
+    '''Extract size of the offset from a two-byte sequence'''
+    raise NotImplementedError
+
+def parse_cli_query(filename, q_type, q_domain, q_server=None) -> tuple:
+    '''Parse command-line query'''
+    raise NotImplementedError
+
+def format_query(q_type: int, q_domain: list) -> bytearray:
+    '''Format DNS query'''
+    raise NotImplementedError
+
+def send_request(q_message: bytearray, q_server: str) -> bytes:
+    '''Contact the server'''
+    client_sckt = socket(AF_INET, SOCK_DGRAM)
+    client_sckt.sendto(q_message, (q_server, PORT))
+    (q_response, _) = client_sckt.recvfrom(2048)
+    client_sckt.close()
+    
+    return q_response
+
+def parse_response(resp_bytes: bytes):
+    '''Parse server response'''
+    raise NotImplementedError
+
+def parse_answers(resp_bytes: bytes, offset: int, rr_ans: int) -> list:
+    '''Parse DNS server answers'''
+    raise NotImplementedError
+
+def parse_address_a(addr_len: int, addr_bytes: bytes) -> str:
+    '''Extract IPv4 address'''
+    raise NotImplementedError
+
+def parse_address_aaaa(addr_len: int, addr_bytes: bytes) -> str:
+    '''Extract IPv6 address'''
+    raise NotImplementedError
+
+def resolve(query: str) -> None:
+    '''Resolve the query'''
+    q_type, q_domain, q_server = parse_cli_query(*query[0])
+    query_bytes = format_query(q_type, q_domain)
+    response_bytes = send_request(query_bytes, q_server)
+    answers = parse_response(response_bytes)
+    print('DNS server used: {}'.format(q_server))
+    for a in answers:
+        print('Domain: {}'.format(a[0]))
+        print('TTL: {}'.format(a[1]))
+        print('Address: {}'.format(a[2]))
+
+def main(*query):
+    '''Main function'''
+    if len(query[0]) < 3 or len(query[0]) > 4:
+        print('Proper use: python3 resolver <type> <domain> <server>')
+        exit()
+    resolve(query)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
