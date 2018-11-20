235192f0d6604b1375591385aeee7d42c02b9310
Ahmad M. Osman <ahmad.osman@luther.edu>
2018-11-19 18:37:45 -0600

    Finished

--- project5/pinger.py
+++ project5/pinger.py
@@ -75,13 +75,58 @@ def parse_reply(my_socket: socket.socket, req_id: int, timeout: int, addr_dst: s
         pkt_rcvd, addr = my_socket.recvfrom(1024)
         if addr[0] != addr_dst:
             raise ValueError(f"Wrong sender: {addr[0]}")
+        
         # TODO: Extract ICMP header from the IP packet and parse it
+        
         # print_raw_bytes(pkt_rcvd)
+        
+        pkt_size = len(pkt_rcvd)
+        ICMP_header = pkt_rcvd[20:28]
+        ICMP_type, code, chksum, pkt_id, seq_num = struct.unpack("bbHHh", ICMP_header)
+        
+        # print_raw_bytes(ICMP_header)
+        # print_raw_bytes(struct.unpack("bbHHh", ICMP_header))
+        
+        if ICMP_type != ECHO_REPLY_TYPE:
+            raise ValueError(f"Wrong ICMP Type: {ICMP_type}")
+        elif code != ECHO_REPLY_CODE:
+            raise ValueError(f"Wrong Code Type: {code}")
+        elif pkt_id != req_id:
+            raise ValueError("Request ID is not the same as the Packet ID")
+
+        bts = struct.calcsize("d")
+        timestamp = struct.unpack("d", pkt_rcvd[28:28 + bts])[0]
+        rtt = time_rcvd - timestamp
+
+        ttl = pkt_rcvd[8]
+
+        my_checksum = 0
+        hdr = struct.pack("bbHHh", ICMP_type, code, my_checksum, pkt_id, seq_num)
+        my_checksum = checksum(hdr + pkt_rcvd[28:])
+
+        v = list(my_checksum.to_bytes(2, byteorder='big'))
+        my_hex_chksum = hex(v[0]).replace("0x", '') + hex(v[1]).replace("0x", '')
+
+        chksum_raw_bytes = pkt_rcvd[22:24]
+
+        tmp = []
+        for i in range(len(chksum_raw_bytes)):
+            tmp.append("{:02x} ".format(chksum_raw_bytes[i]))
+        
+        tmp = ''.join(tmp)
+        tmp = tmp.replace(' ', '')
+        # print(tmp)
+
+        if my_hex_chksum != tmp:
+            raise ValueError("Incorrect Checksum")
+        
         # DONE: End of ICMP parsing
+        
         time_left = time_left - how_long_in_select
         if time_left <= 0:
             raise TimeoutError("Request timed out after 1 sec")
 
+        return (addr_dst, pkt_size, rtt, ttl, seq_num)
 
 def format_request(req_id: int, seq_num: int) -> bytes:
     """Format an Echo request"""
@@ -117,7 +162,7 @@ def send_request(addr_dst: str, seq_num: int, timeout: int = 1) -> tuple:
     try:
         result = parse_reply(my_socket, my_id, timeout, addr_dst)
     except ValueError as ve:
-        print(f"Packet error: {ve}")
+        raise(f"Packet error: {ve}")
     finally:
         my_socket.close()
     return result
@@ -130,7 +175,43 @@ def ping(host: str, pkts: int, timeout: int = 1) -> None:
         Displays host statistics. You need to modify lines between labels TODO and DONE.
     """
     # TODO: Implement the main loop
+    addr_dst = socket.gethostbyname(host)
+    
+    print()
+    print("--- Ping", host, '(' + addr_dst + ')', "using Python ---")
+    print()
 
+    pkts_lost = 0
+    rttlst = []
+    for i in range(pkts):
+        result = ""
+        try:
+            result = send_request(addr_dst, seq_num = i, timeout = timeout)
+            rttlst.append(round(result[2] * pow(10, 3), 2))
+            print(result[1], "bytes from", str(result[0]) + ": icmm_seq=" + str(result[4] + 1), "TTL=" + str(result[3]), "time=" + str(round(result[2] * pow(10, 3), 2)), "ms")
+        except TimeoutError as e:
+            result = "No response: " + str(e)
+            pkts_lost += 1
+            print(result)
+        except Exception as e:
+            pkts_lost += 1
+            print("Error:", e)
+    
+    print()
+    print("---", host, "ping statistics ---")
+    
+    if pkts_lost == pkts:
+        print(pkts, "packets transmitted,", pkts-pkts_lost, "received, 100% packet loss")
+    else:
+        print(pkts, "packets transmitted,", pkts-pkts_lost, "received,", str(int(pkts_lost/pkts * 100)) + "% packet loss")
+        try:
+            rttMin = round(min(rttlst), 3)
+            rttMax = round(max(rttlst), 3)
+            rttAvg = round(mean(rttlst), 3)
+            rttStdev = round(stdev(rttlst), 3)
+            print(f"rtt min/avg/max/mdev = {rttMin}/{rttAvg}/{rttMax}/{rttStdev} ms")
+        except:
+            pass
     # DONE
     return
 
