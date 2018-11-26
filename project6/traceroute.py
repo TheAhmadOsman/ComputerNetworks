0bc71d24fb3cd64e358e6ef0b2e7531cc4c0d117
Ahmad M. Osman <ahmad.osman@luther.edu>
2018-11-25 23:30:05 -0600

    progress

--- project6/traceroute.py
+++ project6/traceroute.py
@@ -1,36 +1,88 @@
+"""Python traceroute implementation"""
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+import os
+import select
+import socket
+import struct
+import sys
+import time
+
+ATTEMPTS = 3
+ECHO_REQUEST_CODE = 0
+ECHO_REQUEST_TYPE = 8
+MAX_HOPS = 30
+TIMEOUT = 1
 
 
+def print_raw_bytes(pkt: bytes) -> None:
+    """Print the packet bytes"""
+    for i in range(len(pkt)):
+        sys.stdout.write("{:02x} ".format(pkt[i]))
+        if (i + 1) % 16 == 0:
+            sys.stdout.write("\n")
+        elif (i + 1) % 8 == 0:
+            sys.stdout.write("  ")
+    sys.stdout.write("\n")
 
 
+def checksum(pkt: bytes) -> int:
+    """Calculate and return checksum"""
+    csum = 0
+    count = 0
+    count_to = (len(pkt) // 2) * 2
+
+    while count < count_to:
+        this_val = (pkt[count + 1]) * 256 + (pkt[count])
+        csum = csum + this_val
+        csum = csum & 0xFFFFFFFF
+        count = count + 2
+
+    if count_to < len(pkt):
+        csum = csum + (pkt[len(pkt) - 1])
+        csum = csum & 0xFFFFFFFF
+
+    csum = (csum >> 16) + (csum & 0xFFFF)
+    csum = csum + (csum >> 16)
+    result = ~csum
+    result = result & 0xFFFF
+    result = result >> 8 | (result << 8 & 0xFF00)
+
+    return result
 
 
+def format_request(icmp_type: int, icmp_code: int, req_id: int, seq_num: int) -> bytes:
+    """Format an Echo request"""
 
 
+def main(args):
 
 
+def parse_reply(packet: bytes) -> bool:
+    """Parse an ICMP reply"""
+    return True
 
 
+def receive_reply(open_socket: socket, timeout: int = 1) -> tuple:
+    """Receive an ICMP reply"""
 
 
+def send_request(packet: bytes, addr_dst: str, ttl: int) -> socket:
+    """Send an Echo Request"""
 
 
+def traceroute(hostname: str) -> None:
+    """Trace the route to a domain"""
 
 
+return (pkt_rcvd, addr[0])
+return header + data
+return my_icmp_socket
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main(sys.argv)
+    return
 
                 continue
                 continue
@@ -57,8 +109,6 @@
             print(f"{delim:3s} {v_error_msg}")
             print(f"{rtt:>5.0f} ms", end="")
             rtt = (time_rcvd - time_sent) * 1000
-            sys.stdout.write("  ")
-            sys.stdout.write("\n")
             time_rcvd = time.time()
             time_sent = time.time()
             to_error_msg = ""
@@ -68,17 +118,10 @@
         "bbHHh", icmp_header
         "bbHHh", icmp_type, icmp_code, calculated_checksum, req_id, seq_num
         )
-        count = count + 2
-        csum = csum & 0xFFFFFFFF
-        csum = csum & 0xFFFFFFFF
-        csum = csum + (pkt[len(pkt) - 1])
-        csum = csum + this_val
-        elif (i + 1) % 8 == 0:
         elif v_error_msg:
         else:
         f"Tracing route to {hostname} [{dest_addr}] over a maximum of {MAX_HOPS} hops\n"
         for att in range(ATTEMPTS):
-        if (i + 1) % 16 == 0:
         if responder == dest_addr:
         if to_error_msg:
         parsed_success = 0
@@ -90,16 +133,8 @@
         raise ValueError(f"Incorrect checksum: {check_sum_rcvd}")
         received_success = 0
         sys.exit(1)
-        sys.stdout.write("{:02x} ".format(pkt[i]))
-        this_val = (pkt[count + 1]) * 256 + (pkt[count])
         traceroute(args[1])
-    """Calculate and return checksum"""
-    """Format an Echo request"""
-    """Parse an ICMP reply"""
-    """Print the packet bytes"""
-    """Receive an ICMP reply"""
-    """Send an Echo Request"""
-    """Trace the route to a domain"""
+    """"""
     )
     )
     )
@@ -107,17 +142,11 @@
     calculated_checksum = socket.htons(checksum(chk_header + data))
     check_sum_comptd = checksum(pseudo_header + icmp_data)
     chk_header = struct.pack("bbHHh", icmp_type, icmp_code, 0, req_id, seq_num)
-    count = 0
-    count_to = (len(pkt) // 2) * 2
-    csum = (csum >> 16) + (csum & 0xFFFF)
-    csum = 0
-    csum = csum + (csum >> 16)
     data = struct.pack("d", time.time())
     delim = " "
     dest_addr = socket.gethostbyname(hostname)
     except IndexError:
     expected_types = [0, 3, 11]
-    for i in range(len(pkt)):
     for ttl in range(1, MAX_HOPS + 1):
     header = struct.pack(
     how_long_in_select = time.time() - started_select
@@ -125,7 +154,6 @@
     icmp_header = packet[20:28]
     icmp_msg_type, icmp_msg_code, check_sum_rcvd, repl_id, sequence = struct.unpack(
     if check_sum_rcvd != socket.htons(check_sum_comptd):
-    if count_to < len(pkt):
     if icmp_msg_type not in expected_types:
     if not what_ready[0]:
     if time_left <= 0:
@@ -143,40 +171,8 @@
     pseudo_header.append(0)
     pseudo_header.extend(icmp_header[0:2])
     pseudo_header.extend(icmp_header[4:])
-    result = ~csum
-    result = result & 0xFFFF
-    result = result >> 8 | (result << 8 & 0xFF00)
-    return (pkt_rcvd, addr[0])
-    return header + data
-    return my_icmp_socket
-    return result
-    return True
     started_select = time.time()
-    sys.stdout.write("\n")
     time_left = time_left - how_long_in_select
     time_left = timeout
     try:
     what_ready = select.select([open_socket], [], [], time_left)
-    while count < count_to:
-"""Python traceroute implementation"""
-#!/usr/bin/env python3
-ATTEMPTS = 3
-def checksum(pkt: bytes) -> int:
-def format_request(icmp_type: int, icmp_code: int, req_id: int, seq_num: int) -> bytes:
-def main(args):
-def parse_reply(packet: bytes) -> bool:
-def print_raw_bytes(pkt: bytes) -> None:
-def receive_reply(open_socket: socket, timeout: int = 1) -> tuple:
-def send_request(packet: bytes, addr_dst: str, ttl: int) -> socket:
-def traceroute(hostname: str) -> None:
-ECHO_REQUEST_CODE = 0
-ECHO_REQUEST_TYPE = 8
-if __name__ == "__main__":
-import os
-import select
-import socket
-import struct
-import sys
-import time
-MAX_HOPS = 30
-TIMEOUT = 1
