c8523d1d3984a3c666fb1eeef57ff9d4d145170f
Ahmad M. Osman <ahmad.osman@luther.edu>
2018-12-02 06:48:17 -0600

    Distance Vector implemented while reading file

--- final_project/router_1.py
+++ final_project/router_1.py
@@ -13,8 +13,10 @@ import time
 
 HOST_ID = os.path.splitext(__file__)[0].split("_")[-1]
 THIS_NODE = f"127.0.0.{HOST_ID}"
-PORT = 4300
+PORT = int("4300" + HOST_ID)
+MINE = {}
 NEIGHBORS = set()
+NEIGHBORS_EXTENDED = {}
 ROUTING_TABLE = {}
 TIMEOUT = 5
 MESSAGES = [
@@ -25,9 +27,83 @@ MESSAGES = [
     "Precise Pangolin"
 ]
 
+
 def read_file(filename: str) -> None:
-    """Read config file"""
-    raise NotImplementedError
+    """
+        Read config file.
+
+        We start with a simple application that reads a router's configuration from a text file, displays its status (neighbors and cost of getting to them), and starts listening for incoming UDP connections on port 4300. The configuration contains names of your directly connected neighbors and the cost to reach those neighbors.
+
+        You should write 4 identical files, each one for a different address (127.0.0.x) and port (4300x). By the end of the project you should be able to test your routers locally, at the very least.
+
+        Configuration file format
+            Router_1_IP_address
+            Neighbor_1_IP_addres Cost_of_getting_to_neighbor_1
+            Neighbor_2_IP_addres Cost_of_getting_to_neighbor_2
+
+            Router_2_IP_address
+            Neighbor_1_IP_addres Cost_of_getting_to_neighbor_1
+            Neighbor_2_IP_addres Cost_of_getting_to_neighbor_2
+            Neighbor_3_IP_addres Cost_of_getting_to_neighbor_3
+
+        Read a configuration file for your specific router and add each neighbor to a set of neighbors.
+        Build an initial routing table as a dictionary with nodes as keys.
+        Dictionary values should be a distance to the node and the next hop address (ie. {'destination':[cost, 'next_hop']}).
+        Initially, the dictionary must contain your neighbors only.
+    """
+
+    skip = False
+    current_neighbor = ""
+    with open(filename, "r") as f:
+        for line in f:
+            line = line.split()
+
+            if len(line) == 0:
+                skip = False
+                continue
+            elif len(line) == 1:
+                if line[0] == THIS_NODE:
+                    skip = True
+                    continue
+                current_neighbor = line[0]
+            elif len(line) == 2:
+                if skip:
+                    NEIGHBORS.add(line[0])
+                    MINE[line[0]] = line[1]
+                    ROUTING_TABLE[line[0]] = [line[1], None]
+                    continue
+                if not current_neighbor in NEIGHBORS_EXTENDED:
+                    NEIGHBORS_EXTENDED[current_neighbor] = []
+                NEIGHBORS_EXTENDED[current_neighbor].append(
+                    [line[0], line[1]])
+
+        # print(MINE)
+        # print(NEIGHBORS)
+        # print(NEIGHBORS_EXTENDED)
+
+    print(THIS_NODE)
+
+    print(ROUTING_TABLE)
+
+    # Distance Vector Algorithm
+    for neighbor in NEIGHBORS:
+        neighbor_cost = int(MINE[neighbor])
+        # print("\t Neighbor:", neighbor, "cost:", neighbor_cost)
+        for node in NEIGHBORS_EXTENDED[neighbor]:
+            if THIS_NODE == node[0]:
+                continue
+
+            hop_cost = int(node[1]) + neighbor_cost
+
+            if (not node[0] in ROUTING_TABLE) or (int(ROUTING_TABLE[node[0]][0]) > hop_cost):
+                ROUTING_TABLE[node[0]] = [hop_cost, neighbor]
+
+            # print("\t\t   ", node[0], "cost", hop_cost)
+
+        # break
+    # print(NEIGHBORS_EXTENDED[neighbor])
+
+    print(ROUTING_TABLE)
 
 
 def format_update():
@@ -67,7 +143,7 @@ def print_status():
 
 def main(args: list):
     """Router main loop"""
-    raise NotImplementedError
+    read_file(args[1])
 
 
 if __name__ == "__main__":
